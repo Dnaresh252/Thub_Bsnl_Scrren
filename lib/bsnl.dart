5d44dd3061c65b64ab447f534e77da4f6aa0309b
Dnaresh252 <22p31a0586@acet.ac.in>
2025-02-24 11:40:47 +0530

    Initial commit for Thub_Bsnl_Scrren

---
+++ lib/bsnl.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+class Bsnl extends StatefulWidget {
+  const Bsnl({super.key});
+
+  @override
+  State<Bsnl> createState() => _BsnlState();
+}
+
+class _BsnlState extends State<Bsnl> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+      Container(
+        height: 300,
+      width: MediaQuery.of(context).size.width/0.1,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.orange.shade50, Colors.orange.shade600],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+        child: Column(
+
+        children: [
+          SizedBox(height: 40,),
+          Padding(
+            padding: EdgeInsets.only(left: 20,top: 20),
+            child: Row(
+
+              children: [
+                Text(
+                  "Good Afternoon, Prasad TNVD",
+                  style: TextStyle(color: Colors.black, fontSize: 10, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Padding(
+            padding:EdgeInsets.only(left: 20,top:0) ,
+            child: Row(
+              children: [
+                Text("9618566951", style: TextStyle(color: Colors.black, fontSize: 10,fontWeight: FontWeight.bold)),
+                SizedBox(width: 2),
+                Container(
+                  height: 16,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(child: Text("Prepaid", style: TextStyle(color: Colors.orange, fontSize: 9))),
+                ),
+              ],
+            ),
+          ),
+        // Recharge Card
+          SizedBox(height: 10,),
+        Container(
+          margin: EdgeInsets.all(16),
+          padding: EdgeInsets.all(16),
+          height: 160,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(17),
+            boxShadow: [
+              BoxShadow(color: Colors.grey, blurRadius: 6, spreadRadius: 1)
+            ],
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left:100.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.lte_plus_mobiledata_sharp,color: Colors.black,),
+                    SizedBox(width: 3,),
+                    Icon(Icons.exposure_minus_1_outlined,color: Colors.red,),
+                    Text("0 Pack", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              Text("Expired", style: TextStyle(color: Colors.red, fontSize: 12,fontWeight: FontWeight.bold)),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Container(
+                    height: 40,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2,color: Colors.orange.shade200),
+                      borderRadius: BorderRadius.circular(20)
+                      ),
+                    child: Center(child: Text("View Pack",style: TextStyle(color: Colors.orange),)),
+                    ),
+                   SizedBox(width: 15),
+                  Container(
+                    height: 40,
+                    width: 100,
+                    decoration: BoxDecoration(
+                       color: Colors.orange.shade600,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(child: Text("View Pack",style: TextStyle(color: Colors.white),)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        ],
+
+        ),
+      ),
+          // Exclusive Offer Banner
+
+          SizedBox(height: 10,),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue.shade50, Colors.blue.shade200],
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+              ),
+              borderRadius: BorderRadius.circular(17),
+            ),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("App Exclusive Offer", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 5),
+                    Text("Applicable on recharges above Rs 249.",style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500)),
+                    SizedBox(height: 7),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Check Now >>", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 18)),
+                        // Text("Get 2% OFF", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.orange)),
+                      ],
+                    ),
+                    Text("T&C apply",style: TextStyle(color: Colors.blue,fontSize: 9),),
+                  ],
+                ),
+                 SizedBox(width: 30),
+                Row(
+                  children: [
+                    Text("2", style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold, color: Colors.orange)),
+                    // Text("%",style: TextStyle(color: Colors.black,fontSize: 9),),
+                    Column(
+                      children: [
+                        Text("Get", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)),
+                        Text("OFF", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+
+            ),
+          ),
+         Container(
+           height: 300,
+           width:  390,
+
+
+
+           decoration: BoxDecoration(
+             color: Color.fromRGBO(254, 251, 246,1),
+             borderRadius: BorderRadius.circular(17),
+             boxShadow: [
+               BoxShadow(color: Colors.grey, blurRadius: 3, spreadRadius: 1)
+             ],
+           ),
+           child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+               Padding(
+                 padding: const EdgeInsets.only(left: 8.0),
+                 child: Text("Quick Actions", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+               ),
+
+                 Padding(
+                   padding: const EdgeInsets.only(bottom: 20.0),
+                   child: GridView.count(
+                     crossAxisCount: 4,
+                     shrinkWrap: true,
+                     physics: NeverScrollableScrollPhysics(),
+                     children: [
+                       _quickAction(Icons.refresh, "Recharge"),
+                       _quickAction(Icons.payment, "Pay Bill"),
+                       _quickAction(Icons.phone, "Landline"),
+                       _quickAction(Icons.wifi, "Book Fiber"),
+                       _quickAction(Icons.sim_card, "Upgrade to 4G SIM"),
+                       _quickAction(Icons.numbers, "Number", isNew: true),
+                       _quickAction(Icons.notifications_off, "Do Not Disturb"),
+                       _quickAction(Icons.videogame_asset, "Games"),
+                     ],
+                   ),
+                 ),
+         ]
+         ),
+         )
+        ],
+      ),
+
+
+    );
+  }
+  Widget _quickAction(IconData icon, String title, {bool isNew = false}) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey.shade200,
+              radius: 30,
+              child: Icon(icon, color: Colors.black, size: 20,),
+            ),
+            // if (isNew)
+            //   Positioned(
+            //     right: 0,
+            //     top: 0,
+            //     child: Container(
+            //       padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+            //       decoration: BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(8)),
+            //       child: Text("New", style: TextStyle(color: Colors.white, fontSize: 10)),
+            //     ),
+            //   ),
+          ],
+        ),
+        SizedBox(height: 5),
+        Text(title, style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold)),
+      ],
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+import 'bsnl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Bsnl()
+    );
+  }
+}
+
+class SliverComparisonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sliver vs. ListView Comparison")),
+      body: Row(
+        children: [
+          // SLIVER-BASED SCROLLING (LEFT SIDE)
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                // SliverAppBar
+                SliverAppBar(
+                  expandedHeight: 150.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text("Sliver AppBar"),
+                    background: Image.network(
+                      "https://source.unsplash.com/random",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // SliverPersistentHeader: Sticky Header
+                SliverPersistentHeader(
+                  pinned: true,
+                  delegate: CustomSliverHeaderDelegate(
+                    minHeight: 50,
+                    maxHeight: 80,
+                    child: Container(
+                      color: Colors.blue,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Sliver Sticky Header",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // SliverList
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                        (context, index) => Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.orange[300],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Sliver List Item #$index",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    childCount: 15,
+                  ),
+                ),
+
+                // SliverGrid
+                SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                        (context, index) => Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.teal[200],
+                      child: Center(
+                        child: Text(
+                          "Grid #$index",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    childCount: 9,
+                  ),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                    childAspectRatio: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          VerticalDivider(thickness: 2, color: Colors.black),
+
+          // NORMAL LISTVIEW (RIGHT SIDE)
+          Expanded(
+            child: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.purple[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    "ListView Item #$index",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Custom Delegate for SliverPersistentHeader
+class CustomSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  CustomSliverHeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  bool shouldRebuild(CustomSliverHeaderDelegate oldDelegate) {
+    return oldDelegate.minHeight != minHeight ||
+        oldDelegate.maxHeight != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
